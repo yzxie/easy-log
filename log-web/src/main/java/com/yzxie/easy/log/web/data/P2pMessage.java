fcb120c2c051079cfa1a84e2bf9ccb9420ab82e0
xieyizun <xieyizun@gmail.com>
2018-11-04 21:59:49 +0800

    feature：log-web使用websocket实现实时推送信息给web ui.

---
+++ log-web/src/main/java/com/yzxie/easy/log/web/data/P2pMessage.java
@@ -0,0 +1,14 @@
+package com.yzxie.easy.log.web.data;
+
+import lombok.Data;
+
+/**
+ * @author xieyizun
+ * @date 4/11/2018 21:45
+ * @description:
+ */
+@Data
+public class P2pMessage {
+    String content;
+    String toUser;
+}
